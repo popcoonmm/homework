8c9cd1d882e197d2023700f76cd424674889d90b
”yuna” <popcoonmm@gmil.com>
2019-06-12 09:44:59 +0000

    PHP11章

---
+++ PHP11/create.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.profile')  {{--読み込みメソッド--}}
+@section('title','プロフィール') {{--埋め込み--}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィールの新規作成</h2>
+            </div>
+        </div>
+    </div>
+@endsection
